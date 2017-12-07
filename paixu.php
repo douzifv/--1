d8e22a91ce29aa01559787d4b4a1880b988477b6
douzifv <576274911@qq.com>
2017-12-07 08:45:01 +0800

    Add files via upload

---
+++ paixu.php
@@ -0,0 +1,32 @@
+<?php
+/*function cmp($a, $b){ 
+if ($a["vote_num"] == $b["vote_num"]) { 
+return 0; 
+} 
+return ($a["vote_num"] > $b["vote_num"]) ? -1 : 1; 
+} 
+
+$arr = Array 
+( 
+'0' => Array 
+( 
+'o_id' => 1861, 
+), 
+
+'1' => Array 
+( 
+'o_id' => 1844, 
+), 
+
+2 => Array 
+( 
+'o_id' => 1843, 
+) 
+); 
+uasort($arr, 'cmp'); 
+echo '< pre style="text-align:left" >'; 
+print_r ($arr); 
+echo '< / pre >'; */
+
+$a = date("Y年m月d日",time());
+var_dump($a);
